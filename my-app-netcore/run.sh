b74b08962a20fa4782d3744425a4d80515eb00f0
Marcos de Matos <marck010@hotmail.com>
2018-02-21 00:54:49 -0300

    Limit of memory implemented for containers

--- my-app-netcore/run.sh
+++ my-app-netcore/run.sh
@@ -3,4 +3,4 @@
 sudo docker build -t my-app-netcore .
 sudo docker stop my-app-netcore   
 sudo docker rm my-app-netcore
-sudo docker run --name my-app-netcore -v `pwd`/opt/my-app-netcore  -d my-app-netcore
+sudo docker run -m 200m --name my-app-netcore -v `pwd`/opt/my-app-netcore  -d my-app-netcore
